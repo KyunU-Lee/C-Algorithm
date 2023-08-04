fe860677641874fbc55fc1d8c232dcd09420dde4
KyunU-Lee <rbsdn3159@gmail.com>
2023-08-04 15:53:48 +0900

    list 마무리

--- Algorithm with C/Algorithm with C/list.cpp
+++ Algorithm with C/Algorithm with C/list.cpp
@@ -3,8 +3,9 @@
 #include <cstdlib>
 
 
-void list_init(List* list, void (*destroy) (void* data))
+void list_init(List* list, void (*destroy) (void* data)) 
 {
+	//연결 리스트를 초기화 함 
 	list->size = 0;
 	list->destroy = destroy;
 	list->head = NULL;
@@ -27,32 +28,33 @@ void list_destroy(List* list)
 
 }
 
-int list_ins_next(List* list, ListElmt* element, const void* data)
+int list_ins_next(List* list, ListElmt* element, const void* data) // element NULL 새항목 추가
 {
 	ListElmt* new_element;
-
+	//새로운 element 메모리 생성
 	if ((new_element = (ListElmt*)malloc(sizeof(ListElmt))) == NULL) {
 		return -1;
 	}
-
+	//데이터 삽입
 	new_element->data = (void*)data;
-
-	if (element == NULL) {
-		if (list_size(list) == 0) {
-			list->tail == new_element;
+	//비어있는 List인가? (head에 삽입)
+	if (element == NULL) { //element에 NULL(항목 추가)을 입력하면 head에 데이터를 삽입하라. 
+		if (list_size(list) == 0) {//첫번째 데이터 라면 tail도 [element][next]
+			list->tail = new_element;
 		}
-
+		//다음을 가리키는 포인터가 head를 가리킴
 		new_element->next = list->head;
+		//head에 새로운 데이터가 입력됨
 		list->head = new_element;
 
 	}
 	else {
-
-		if (element->next == NULL) {
+		// tail 인가?
+		if (element->next == NULL) { //새항목 추가 인가?
 			list->tail = new_element;
 		}
-
-		new_element->next = element->next;
+		//기존의 next 포인터를 새로 만들어진 포인터로 바꿔줌
+		new_element->next = element->next; //NULL을 가리킴? 이해잘안됨 
 		element->next = new_element;
 	}
 	list->size++;
@@ -60,7 +62,40 @@ int list_ins_next(List* list, ListElmt* element, const void* data)
 	return 0;
 }
 
-int list_rem_next(List* list, ListElmt* element, const void** data)
+//다음 리스트 제거
+int list_rem_next(List* list, ListElmt* element, void** data) // element NULL -> 삭제
 {
+	// 임시 element
+	ListElmt* old_element;
+	//빈리스트 삭제 X
+	if (list_size(list) == 0)
+		return -1;
+	//Head 삭제 
+	if (element == NULL) { //삭제 명령
+		*data = list->head->data;
+		old_element = list->head;
+		list->head = list->head->next;
+
+		if (list_size(list) == 0)
+			list->tail = NULL;
+	}
+	else
+	{
+		if (element->next == NULL) { // 삭제가 아니라면
+			return -1;
+		}
+
+		*data = element->next->data;
+		old_element = element->next;
+		element->next = element->next->next;
+
+		if (element->next == NULL)
+			list->tail = element;
+	}
+
+	free(old_element);
+
+	list->size--;
+
 	return 0;
 }
--- Algorithm with C/Algorithm with C/list.h
+++ Algorithm with C/Algorithm with C/list.h
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 
 typedef struct ListElmt_ { //연결리스트 항목
-	void*				data;
-	struct ListElmt_*	next;
+	void*				data; 
+	struct ListElmt_*	next; //다음을 가리키는 포인터
 }ListElmt;
 
 typedef struct List_ {
-	int size;
-	int (*match)(const void* key1, const void* key2);
-	void (*destroy)(void* data);
+	int size; //list 크기
+	int (*match)(const void* key1, const void* key2); //파생되는 자료형에서 사용될 예정
+	void (*destroy)(void* data); // list_init에 전달 될 캡슐화된 제거 함수
 
-	ListElmt	*head;
-	ListElmt	*tail;
+	ListElmt	*head; //첫번째 항목을 가리키는 포인터
+	ListElmt	*tail; // 마지막 항목을 가리키는 포인터 
 } List;
 
 void list_init(List* list, void (*destroy)(void* data));
@@ -22,7 +22,7 @@ void list_destroy(List* list);
 
 int list_ins_next(List* list, ListElmt* element, const void* data);
 
-int list_rem_next(List* list, ListElmt* element, const void** data);
+int list_rem_next(List* list, ListElmt* element, void** data);
 
 #define list_size(list) ((list)->size)
 
